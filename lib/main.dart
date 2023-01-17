3ff4697201b5980c99e16717588954a54aec7cc3
Niharkumar Shah <niharkumarshah@gmail.com>
2023-01-17 12:15:00 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'screens/note_list.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'LCO ToDo App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: NoteList(),
+    );
+  }
+}
---
+++ lib/screens/note_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NoteList extends StatefulWidget {
+  const NoteList({super.key});
+
+  @override
+  State<NoteList> createState() => _NoteListState();
+}
+
+class _NoteListState extends State<NoteList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
