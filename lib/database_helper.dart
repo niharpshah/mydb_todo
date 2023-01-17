60c533b082943129b65b52d9cdd4cb3ece56c903
Niharkumar Shah <niharkumarshah@gmail.com>
2023-01-17 21:30:17 +0530

    Database Helper created and screens

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -5,8 +5,8 @@ import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 
 class DatabaseHelper {
-  static late DatabaseHelper _databaseHelper; //Singleton
-  static late Database _database; // Singleton
+  static DatabaseHelper? _databaseHelper; //Singleton
+  static Database? _database; // Singleton
 
   String noteTable = 'note_table';
   String colId = 'id';
@@ -18,13 +18,13 @@ class DatabaseHelper {
   DatabaseHelper._createInstance();
 
   factory DatabaseHelper() {
-    if (DatabaseHelper == null) {
+    if (_databaseHelper == null) {
       _databaseHelper = DatabaseHelper._createInstance();
     }
-    return _databaseHelper;
+    return _databaseHelper!;
   }
   // custom getter
-  Future<Database> get database async {
+  Future<Database?> get database async {
     if (_database == null) {
       _database = await initializeDatabase();
     }
@@ -49,37 +49,37 @@ class DatabaseHelper {
   }
 
   Future<List<Map<String, dynamic>>> getNoteMapList() async {
-    Database db = await this.database;
+    Database? db = await this.database;
     // regular MySQL query optional
     // var result =
     //     await db.rawQuery('SELECT * from $noteTable order by $colPriority ASC');
-    var result = await db.query(noteTable, orderBy: '$colPriority ASC');
+    var result = await db!.query(noteTable, orderBy: '$colPriority ASC');
     return result;
   }
 
   Future<int> insertNote(Note note) async {
-    Database db = await this.database;
-    var result = await db.insert(noteTable, note.toMap());
+    Database? db = await this.database;
+    var result = await db!.insert(noteTable, note.toMap());
     return result;
   }
 
   Future<int> updateNote(Note note) async {
-    Database db = await this.database;
-    var result = await db.update(noteTable, note.toMap(),
+    Database? db = await this.database;
+    var result = await db!.update(noteTable, note.toMap(),
         where: '$colId=?', whereArgs: [note.id]);
     return result;
   }
 
   Future<int> deleteNote(int id) async {
-    Database db = await this.database;
-    var result = await db.rawDelete('DELETE FROM $noteTable where $colId=$id');
+    Database? db = await this.database;
+    var result = await db!.rawDelete('DELETE FROM $noteTable where $colId=$id');
     return result;
   }
 
   Future<int?> getCount() async {
-    Database db = await this.database;
+    Database? db = await this.database;
     List<Map<String, dynamic>> x =
-        await db.rawQuery('SELECT COUNT (*) from $noteTable');
+        await db!.rawQuery('SELECT COUNT (*) from $noteTable');
     int? result = Sqflite.firstIntValue(x);
     return result;
   }
--- lib/screens/note_list.dart
+++ lib/screens/note_list.dart
@@ -13,8 +13,37 @@ class NoteList extends StatefulWidget {
 }
 
 class _NoteListState extends State<NoteList> {
+  DatabaseHelper databaseHelper = DatabaseHelper();
+  late List<Note> noteList;
+  int count = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('LCO ToDo'),
+        backgroundColor: Colors.purple,
+      ),
+      body: Container(),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.purple,
+        child: Icon(Icons.add),
+        onPressed: () {
+          navigatetoDetail(Note('', '', 2), 'Add Note');
+        },
+      ),
+    );
+  }
+
+  void navigatetoDetail(Note note, String title) async {
+    bool result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) {
+        return NoteDetail(note, title);
+      }),
+    );
+    if (result == true) {
+      // TODO: update the view
+    }
   }
 }
